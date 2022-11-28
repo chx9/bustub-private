07eaa9c5377cf5314117061aaba8e0e57c70f786
chx9 <working.huan@gmail.com>
2022-11-28 21:31:05 +0800

    project3 update

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,38 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include <memory>
+#include "common/config.h"
+#include "concurrency/lock_manager.h"
+#include "recovery/log_manager.h"
+#include "storage/table/table_heap.h"
+#include "storage/table/table_iterator.h"
 
 namespace bustub {
 
 SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() { 
+    table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+    table_iterator_ = std::make_unique<TableIterator>(table_info_->table_->Begin(exec_ctx_->GetTransaction()));
+ }
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
-
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+    if(*table_iterator_ == table_info_->table_->End()){
+        return false;
+    }
+    Tuple raw_tuple;
+    raw_tuple = *(*table_iterator_);
+    ++(*table_iterator_); 
+    std::vector<Value> values{};
+    values.reserve(GetOutputSchema().GetColumnCount());
+      std::transform(GetOutputSchema().GetColumns().begin(), GetOutputSchema().GetColumns().end(),
+                 std::back_inserter(values), [&raw_tuple, &table_info = table_info_](const Column &col) {
+                   return col.GetExpressions()->Evaluate(&raw_tuple, &(table_info->schema_));
+                 });
+ 
+    *tuple = Tuple{values, &GetOutputSchema()};
+    *rid = tuple->GetRid();
+    return true;
+ }
 }  // namespace bustub
